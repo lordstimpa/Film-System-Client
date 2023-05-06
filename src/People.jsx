ebe875f6d0353dc1a410258229292ece8d6406f3
Steven Dalfall <steven.dalfall@live.se>
2023-05-06 02:25:29 +0200

    Added ability to navigate to person page

--- src/People.jsx
+++ src/People.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import FetchData from "./FetchData";
 import Styled from "styled-components";
-import axios from "axios";
 import { Link } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 
@@ -8,49 +7,52 @@ const PersonOuterContainer = Styled.div`
     display: flex;
     flex-direction: column;
     text-decoration: none;
-
     & .person-inner-container {
         padding-bottom: 1em;
     }
-
     & .person-link {
         text-decoration: none;
         color: inherit;
-    }
-    
+        font-size: 1.2em;
+    } 
     & .person-link:hover {
         color: #2bff2b;
+    }
+    & .loading {
+        color: #2bff2b;
+        text-align: center;
+        font-size: 1.2em;
+        padding-top: 2em;
+    }
+    & .error {
+        color: #ff2b2b;
+        text-align: center;
+        font-size: 1.2em;
+        padding-top: 2em;
     }
 `;
 
 const People = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios("https://localhost:7001/person");
-      console.log(result);
-      setData(result.data);
-    };
-
-    fetchData();
-  }, []);
-
-  const handleClick = () => {};
+  const {
+    data: people,
+    isError,
+    isLoading,
+  } = FetchData("https://localhost:7001/person");
 
   return (
     <PersonOuterContainer>
-      {data.map((item) => (
-        <div key={item.id_person} className="person-inner-container">
-          <Link
-            to={`/person/${item.id_person}`}
-            className="person-link"
-            onClick={handleClick}
-          >
-            <CgProfile /> {item.first_name} {item.last_name}
-          </Link>
-        </div>
-      ))}
+      {isLoading && <div className="loading">Loading people...</div>}
+      {isError && (
+        <div className="error">ERROR: Not able to fetch people data.</div>
+      )}
+      {people &&
+        people.map((item) => (
+          <div key={item.id_person} className="person-inner-container">
+            <Link to={`/person/${item.id_person}`} className="person-link">
+              <CgProfile /> {item.first_name} {item.last_name}
+            </Link>
+          </div>
+        ))}
     </PersonOuterContainer>
   );
 };
--- src/Person.jsx
+++ src/Person.jsx
@@ -1,13 +1,48 @@
 import Styled from "styled-components";
+import FetchData from "./FetchData";
+import { useParams } from "react-router-dom";
 
 const PersonContainer = Styled.div`
     padding: 1em;
+    & .loading {
+        color: #2bff2b;
+        text-align: center;
+        font-size: 1.2em;
+        padding-top: 2em;
+    }
+    & .error {
+        color: #ff2b2b;
+        text-align: center;
+        font-size: 1.2em;
+        padding-top: 2em;
+    }
 `;
 
 const Person = () => {
+  // Get id of person from url parameter
+  const { id } = useParams();
+  const {
+    data: people,
+    isError,
+    isLoading,
+  } = FetchData("https://localhost:7001/person");
+
+  // Find the target person from an array of people
+  const targetPerson = people.find(
+    (person) => person.id_person === parseInt(id)
+  );
+
   return (
     <PersonContainer>
-      <h2>Person</h2>
+      {isLoading && <div className="loading">Loading person...</div>}
+      {isError && (
+        <div className="error">ERROR: Not able to fetch person data.</div>
+      )}
+      {targetPerson && (
+        <h2>
+          {targetPerson.first_name} {targetPerson.last_name}
+        </h2>
+      )}
     </PersonContainer>
   );
 };
